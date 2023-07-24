31adc4c5cea518767f3e2aaf2f7a3658acd72733
Fernando Junior <39204131+fernandojr999@users.noreply.github.com>
2023-07-24 17:22:13 -0300

    Update ClienteController.java

--- src/main/java/com/example/mongodb/mongodb/ClienteController.java
+++ src/main/java/com/example/mongodb/mongodb/ClienteController.java
@@ -1,14 +1,11 @@
 package com.example.mongodb.mongodb;
 
-import ch.qos.logback.core.net.server.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/cliente")
